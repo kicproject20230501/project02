9d8f5c36037ff799457a4fc6f256a1843b6586f2
CrimsonNoel <131672235+CrimsonNoel@users.noreply.github.com>
2023-10-05 23:06:14 +0900

    11
    11모델수정

--- kfoodpro/contents/views.py
+++ kfoodpro/contents/views.py
@@ -19,16 +19,17 @@ def process_image(input_image):
 
 def predict_image(last_file_name):
     food_model = load_food_model()
-    fs = FileSystemStorage()
-    file_list = fs.listdir('')
     input_file_path = os.path.join(settings.MEDIA_ROOT, last_file_name)
     img = tf.keras.utils.load_img(input_file_path)
+    print("==type: ",type(img))
     img_array = tf.keras.utils.img_to_array(img)
+    #img_array = tf.keras.applications.inception_v3.preprocess_input(img_array)
     image_shape = img_array.shape
-    
+    #img_array = img_array / 255.0
+    print("img_array: ",img_array)
     predictions = food_model.predict(img_array)
     score = tf.nn.softmax(predictions[0])
-    img_array = tf.keras.applications.inception_v3.preprocess_input(img_array)
+    
     
     categories = ['001', '002', '003', '004', '005', '006', '007', '008', '009', '010', '011', '012', '013', '014', '015', '016', '017', '018', '019', '020', '021', '022', '023', '024', '025', '026', '027', '028', '029', '030', '031', '032', '033', '034', '035', '036', '037', '038', '039', '040', '041', '042', '043', '044', '045', '046', '047', '048', '049', '050', '051', '052', '053', '054', '055', '056', '057', '058', '059', '060', '061', '062', '063', '064', '065', '066', '067', '068', '069', '070', '071', '072', '073', '074', '075', '076', '077', '078', '079', '080', '081', '082', '083']
     categories_index = ['갈치구이','고등어구이', '더덕구이', '장어구이', '조개구이', '조기구이', '황태구이', '훈제오리', '계란국', '떡국_만두국',
@@ -61,11 +62,12 @@ def start(request):
             print('이미지가 업로드되지 않았습니다.')
             return render(request, 'alert.html', context)
         else:
-            uploaded_file_image = process_image(uploaded_file) # PIL로 바뀌고 저장안되니 날것 이미지로 날것이 정확도 좋음
             fs = FileSystemStorage()
             filename = uploaded_file.name.split('.')[0]+'.jpg'
             file_save = fs.save(filename, uploaded_file)
-            
+            #uploaded_file_image = process_image(uploaded_file)
+            #input_file_path = os.path.join(settings.MEDIA_ROOT, filename)
+            #uploaded_file_image.save(input_file_path)
             return HttpResponseRedirect("/contents/loading/")
     
     return render(request, 'contents/start.html')
@@ -99,9 +101,20 @@ def loading(request):
     category= predict_image(last_file_name)
     print(category)
     context = {"category": category}
+    
     return render(request, 'contents/result.html', context)
 
 def result(request):
+    
+# =============================================================================
+#     # 피드백 아니요시 파일 삭제할부분 
+#     try:
+#         os.remove(input_file_path)
+#         print("삭제")
+#     except:
+#         print("안돼")
+#         pass
+# =============================================================================
     return render(request, 'contents/result.html')
 
 def wrong(request):
